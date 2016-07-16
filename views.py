7498d7fb0216ff6391b0aacddd18f727d66eda73
= <kacamenzind@gmail.com>
2016-07-16 00:59:26 -0700

    Stupid

--- views.py
+++ views.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 def get_file(filename):  # pragma: no cover
     try:
         src = os.path.join('html', filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
         return open(src).read()
     except IOError as exc:
         return str(exc)
